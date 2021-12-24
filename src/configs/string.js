5d0b44db2a02c0537b582ca4787f99759d727d12
Thiện Dương Bá <thiendb@vicoders.com>
2021-12-24 14:43:19 +0700

    Welcome screens done

---
+++ src/configs/string.js
@@ -0,0 +1,7 @@
+export const TEXT = {
+    WELCOME : {
+        WELCOME: 'Welcome!',
+        CONTENT: 'Congratulations on becoming a member of Swinburne Vietnam. Click "Continue" to join.',
+        TEXT_BTN: 'Continue'
+    }
+}
